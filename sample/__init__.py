9b4ac8dfdefcf68a2d66c8a3aeabefc0ccb3f709
Tim Savage <tim@savage.company>
2020-06-08 19:31:11 +1000

    Added sample test app

---
+++ sample/__init__.py
@@ -0,0 +1,3 @@
+"""
+Sample AWS Messaging Application
+"""
---
+++ sample/default_settings.py
@@ -0,0 +1,44 @@
+# Credentials for localstack
+AWS_CREDENTIALS = {
+    "default": {
+        "region": "ap-southeast-2",
+        "aws_access_key_id": "123",
+        "aws_secret_access_key": "123",
+        # "aws_session_token": None,
+    }
+}
+
+
+SEND_MESSAGE_QUEUES = {
+    "sqs": (
+        "pyapp_ext.messaging_aws.aio.SQSSender",
+        {
+            "queue_name": "queue1",
+            "client_args": {"endpoint_url": "http://localhost:4566/"},
+        },
+    ),
+    "sns": (
+        "pyapp_ext.messaging_aws.aio.SNSSender",
+        {
+            "topic_arn": "topic1",
+            "client_args": {"endpoint_url": "http://localhost:4566"},
+        },
+    ),
+}
+
+RECEIVE_MESSAGE_QUEUES = {
+    "sqs": (
+        "pyapp_ext.messaging_aws.aio.SQSReceiver",
+        {
+            "queue_name": "queue1",
+            "client_args": {"endpoint_url": "http://localhost:4566/"},
+        },
+    ),
+    "sns": (
+        "pyapp_ext.messaging_aws.aio.SQSReceiver",
+        {
+            "queue_name": "topic1",
+            "client_args": {"endpoint_url": "http://localhost:4566/"},
+        },
+    ),
+}
