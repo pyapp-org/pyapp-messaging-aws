bc550ad299ba6e65deae7532576210540e5365fe
Tim Savage <tim@savage.company>
2020-06-08 19:33:18 +1000

    Add packaging details

---
+++ setup.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from setuptools import setup
+
+here = Path(__file__).parent
+
+about = {}
+with (here / "pyapp_ext/messaging_aws/__version__.py").open() as f:
+    exec(f.read(), about)
+
+setup(version=about["__version__"])
